03bde870aefe698147b327819143dbc4445be942
Dustin <Dustin@Bischke.ca>
2018-02-04 21:40:57 +0000

    Updated Config Example with Commands

--- settings/config.example.py
+++ settings/config.example.py
@@ -1,8 +1,19 @@
+# Bot Information
 dev_name = 'Silver#4636'
 dev_id = '128905093880741888'
-game = 'Rocket League'
+game = 'INSERT GAME HERE'
 maintenance = False
 maintenance_msg = 'Commands are Temporarily Disabled for Maintenance. Sorry for the Inconvenience!'
 permissions = '0'
-prefix = '!'
+prefix = '!shibe'
 token = 'INSERT BOT TOKEN HERE'
+
+# Enabled Commands
+help = True
+bork = False
+coinflip = True
+eightball = True
+roll = True
+shibe = True
+stats = True
+youtube = True
